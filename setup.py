422290fbf35e41870d12fc2199af5fccce0eabc8
reddy961875 <114823527+reddy961875@users.noreply.github.com>
2023-02-06 08:26:11 +0530

    adding setup.py files and pypi

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     version='0.0.1',
     license='MIT',
     description='Computer Vision Helping Library',
-    author='Computer Vision Zone',
+    author='InfiniAi',
     author_email='info@infiniai.tech',
     url="https://github.com/infiniai-tech/infiniai.git",
     keywords=['ComputerVision', 'HandTracking', 'FaceDetection', 'PoseEstimation','FaceMesh'],
@@ -19,6 +19,10 @@ setup(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-        'python_requires = ">=3.6"'
+        'Programming Language :: Python :: 3.6',  # Specify which pyhton versions that you want to support
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10'
     ],
 )
